32f2d1da5b6ef4b21d31a36457a6f6d1006dfd99
Andreas Kluge-Kaindl <andi@team33.de>
2014-04-21 10:28:55 +0200

    Introduced Scheduler.Builder

--- src/main/java/net/team33/async/consumer/Scheduler.java
+++ src/main/java/net/team33/async/consumer/Scheduler.java
@@ -4,6 +4,7 @@ import java.util.*;
 import java.util.function.Consumer;
 
 import static java.lang.String.format;
+import static java.lang.System.currentTimeMillis;
 import static java.util.Objects.requireNonNull;
 import static net.team33.async.consumer.Payload.payload;
 
@@ -15,6 +16,7 @@ import static net.team33.async.consumer.Payload.payload;
  * @author AKK - Andreas Kluge-Kaindl, Bremen (de)
  * @since team33-async-8.0.1
  */
+@SuppressWarnings("UnusedDeclaration")
 public class Scheduler<MSG> implements Consumer<MSG> {
 
     private static final String TO_STRING_FORMAT = "%s(%s)";
@@ -23,33 +25,18 @@ public class Scheduler<MSG> implements Consumer<MSG> {
     private final Launcher launcher = new Launcher(Thread::new);
     private final Runnable worker = new Worker();
     private final Queue<MSG> queue = new ArrayDeque<>(0);
+    private final Variable variable = new Variable();
+
     private final Strategy strategy;
     private final Consumer<? super MSG> target;
-    /**
-     * The number of started worker threads not finished or may be not even
-     * started working.
-     */
-    private int started = 0;
-    /**
-     * The number of running worker threads not finished but definitely
-     * started working.
-     */
-    private int working = 0;
-    /**
-     * Indicates if the instance is ready to receive (and process) messages.
-     * Initially {@code true}. If not the instance will behave unspecific as
-     * a listener throwing an IllegalStateException when a message is passed.
-     */
-    private boolean ready = true;
 
-    /**
-     * @param strategy A {@link Strategy} used to increase worker threads. Not {@code null}.
-     * @param target   A {@link Consumer} that finally processes incoming messages. Not {@code null}.
-     * @throws NullPointerException if any argument is {@code null}.
-     */
-    public Scheduler(final Strategy strategy, final Consumer<? super MSG> target) throws NullPointerException {
-        this.strategy = requireNonNull(strategy);
-        this.target = requireNonNull(target);
+    private Scheduler(final Builder<MSG> origin) {
+        this.strategy = origin.strategy;
+        this.target = origin.target;
+    }
+
+    public static <MSG> Builder<MSG> builder(final Strategy strategy, final Consumer<MSG> target) {
+        return new Builder<>(strategy, target);
     }
 
     private static void throwProblems(final Iterator<Throwable> iterator) throws Throwable {
@@ -60,10 +47,6 @@ public class Scheduler<MSG> implements Consumer<MSG> {
             }
             throw head;
         }
-    }
-
-    public static Builder builder() {
-        return new Builder();
     }
 
     /**
@@ -92,60 +75,54 @@ public class Scheduler<MSG> implements Consumer<MSG> {
     }
 
     /**
-     * The number of currently running worker threads.
-     * <p/>
+     * The number of currently running worker threads (from scheduler´s point of view), greater or equal to {@code 0}.
+     * <p>
      * Includes currently rising threads not yet really working (but going to).
-     * Excludes those in deed still running but finished processing messages
-     * and so definitely going to terminate.
-     *
-     * @return The number, greater or equal to {@code 0}.
+     * <p>
+     * Excludes those threads in deed still running but definitely finished processing messages
+     * and so definitely are going to terminate.
      */
     public final synchronized int getRunning() {
-        return started;
+        return variable.started;
     }
 
     /**
-     * The number of messages passed and not yet finally processed.
-     *
-     * @return The number, greater or equal to {@code 0}.
+     * The number of messages passed and not yet finally processed, greater or equal to {@code 0}.
      */
     public final synchronized int getLoad() {
-        return queue.size() + working;
+        return queue.size() + variable.working;
     }
 
     /**
-     * The number of messages queued for future processing.
+     * The number of messages queued for future processing, greater or equal to {@code 0}.
      * <p/>
      * Excludes those messages been queued in deed but already dedicated to
      * just rising new worker threads. So the real queue size may be greater
      * than this value (at most by {@link #getRunning()}).
-     *
-     * @return The number, greater or equal to {@code 0}.
      */
     public final synchronized int getOverhead() {
-        return queue.size() + working - started;
+        return queue.size() + variable.working - variable.started;
     }
 
     /**
      * {@inheritDoc}
      * <p/>
-     * <b>This implementation</b> atomically queues the message for
-     * asynchronous processing and starts a new worker thread if appropriate
-     * or necessary.
+     * <b>This implementation</b> atomically queues the message for asynchronous processing
+     * and starts a new worker thread if appropriate or necessary.
      *
      * @param message The message, not {@code null}.
      *
-     * @throws NullPointerException  when {@code message} is {@code null}.
+     * @throws NullPointerException  if {@code message} is {@code null}.
      * @throws IllegalStateException if {@linkplain #stop() stopped} and not
      *                               yet {@linkplain #start() restarted}
      *                               (basically unspecific for Consumers).
      */
     @Override
     public final synchronized void accept(final MSG message) throws NullPointerException, IllegalStateException {
-        if (ready) {
+        if (variable.ready) {
             this.queue.add(message);
-            if (strategy.test(payload(started, queue.size() + working))) {
-                started += 1;
+            if (strategy.test(payload(queue.size() + variable.working, variable.started))) {
+                variable.started += 1;
                 launcher.launch(worker);
             }
         } else {
@@ -154,8 +131,8 @@ public class Scheduler<MSG> implements Consumer<MSG> {
     }
 
     /**
-     * Blocks the calling thread until all queued messages are processed so
-     * that all worker threads should be terminated or a timeout occurs.
+     * Blocks the calling thread until all queued messages are processed (so
+     * that all worker threads should be terminated) or a timeout occurs.
      *
      * @param millis The timeout time in milliseconds.
      * @return {@code true} if all queued messages are processed.
@@ -167,45 +144,36 @@ public class Scheduler<MSG> implements Consumer<MSG> {
             throw new IllegalArgumentException("0 > millis (" + millis + ")");
 
         } else if (0 < millis) {
-            final long time0 = System.currentTimeMillis();
-            for (long delta = 0; (delta < millis) && (0 < started); delta = System.currentTimeMillis() - time0) {
+            final long time0 = currentTimeMillis();
+            for (long delta = 0; (delta < millis) && (0 < variable.started); delta = currentTimeMillis() - time0) {
                 this.wait(millis - delta);
             }
         }
-        return (0 == started);
+        return (0 == variable.started);
     }
 
     /**
      * Called by {@link Worker#run()} to poll a message from the queue.
-     * <p/>
-     * Sorgt dafür dass die Anzahl der gestarteten/laufenden Threads
-     * heruntergezählt wird und benachrichtigt andere Threads, die ggf.
-     * darauf warten, wenn keine weitere Nachricht aus der Warteschlange
-     * geholt werden kann. Der WORKER-Thread ist damit praktisch am Ende.
      *
-     * @return Die nächste Nachricht aus der Warteschlange oder {@code null}
-     * wenn die Warteschlange leer ist.
+     * @return The next queued message or {@code null} if the queue is empty.
      */
     private synchronized MSG poll(final boolean first) {
         if (first) {
-            // This thread now (formally) starts working
-            // (even if the queue is empty) ...
-            working += 1;
+            // This thread now formally starts working (even if the queue might be empty) ...
+            variable.working += 1;
         }
 
         final MSG result = this.queue.poll();
         if (null == result) {
-            // This thread stoppes working (even if just formally started)
-            // and definitely will be going to terminate, so recognize that
-            // right now ...
-            started -= 1;
-            working -= 1;
+            // This thread will stop working (even if just formally started) and definitely will be going to terminate,
+            // so recognize that right now ...
+            variable.started -= 1;
+            variable.working -= 1;
 
-            // Falls jemand darauf wartet, dass (effektiv) kein
-            // WORKER-Thread mehr läuft (z.B. über join()) ...
-            if (1 > started) {
-                assert (0 == started);
-                assert (0 == working);
+            // If 'somebody' is waiting for no worker thread is running ...
+            if (1 > variable.started) {
+                assert (0 == variable.started);
+                assert (0 == variable.working);
                 assert (0 == queue.size());
                 this.notifyAll();
             }
@@ -215,7 +183,7 @@ public class Scheduler<MSG> implements Consumer<MSG> {
     }
 
     /**
-     * Causes the mentioned instance to (re)gain normal operation:
+     * Causes the scheduler to (re)gain normal operation:
      * <ul>
      * <li>Instantly further messages may be {@linkplain #accept(Object) passed}.</li>
      * <li>Processing of {@linkplain #accept(Object) passed} messages will continue as far as
@@ -225,11 +193,11 @@ public class Scheduler<MSG> implements Consumer<MSG> {
      * Does nothing if already started (respectively never {@linkplain #stop() stopped}).
      */
     public final synchronized void start() {
-        ready = true;
+        variable.ready = true;
     }
 
     /**
-     * Causes the mentioned instance to stop normal operation:
+     * Causes the scheduler to stop normal operation:
      * <ul>
      * <li>Instantly no more messages may be {@linkplain #accept(Object) passed}. Any attempt will cause an
      * {@link IllegalStateException}.</li>
@@ -240,15 +208,15 @@ public class Scheduler<MSG> implements Consumer<MSG> {
      * Does nothing if already stopped.
      */
     public final synchronized void stop() {
-        ready = false;
+        variable.ready = false;
     }
 
     /**
-     * Causes the mentioned instance to stop normal operation as soon as possible:
+     * Causes the scheduler to stop normal operation as soon as possible:
      * <ul>
      * <li>Instantly no more messages may be {@linkplain #accept(Object) passed}. Any attempt will cause an
      * {@link IllegalStateException}.</li>
-     * <li>Queued messages not yet in process will be unqueued (and returned)</li>
+     * <li>Queued messages not yet in processing will be un-queued (and returned)</li>
      * <li>Ongoing processing is completed normally, however, until {@link #getLoad()} is finally {@code 0}.</li>
      * </ul>
      * <p/>
@@ -264,10 +232,41 @@ public class Scheduler<MSG> implements Consumer<MSG> {
     }
 
     public final synchronized boolean isStopped() {
-        return !ready;
+        return !variable.ready;
     }
 
-    public static class Builder {
+    private static class Variable {
+        /**
+         * The number of started worker threads not finished or may be not even
+         * started working (started from scheduler´s point of view).
+         */
+        private int started = 0;
+        /**
+         * The number of running worker threads not finished but definitely
+         * started working (started from worker´s point of view).
+         */
+        private int working = 0;
+        /**
+         * Indicates if the instance is ready to receive (and process) messages.
+         * Initially {@code true}. If not the instance will behave unspecific as
+         * a listener throwing an IllegalStateException when a message is passed.
+         */
+        private boolean ready = true;
+    }
+
+    public static class Builder<MSG> {
+
+        private final Strategy strategy;
+        private final Consumer<MSG> target;
+
+        private Builder(final Strategy strategy, final Consumer<MSG> target) throws NullPointerException {
+            this.strategy = requireNonNull(strategy);
+            this.target = requireNonNull(target);
+        }
+
+        public Scheduler<MSG> build() {
+            return new Scheduler<>(this);
+        }
     }
 
     private class Worker implements Runnable {
--- src/test/java/net/team33/async/consumer/SchedulerTest.java
+++ src/test/java/net/team33/async/consumer/SchedulerTest.java
@@ -10,11 +10,10 @@ import java.util.function.Consumer;
 
 public class SchedulerTest {
 
-    @Test
-    public final void test() throws InterruptedException {
+    private static void testAccept(final Strategy strategy, final int size) throws InterruptedException {
         final Aggregator aggregator = new Aggregator();
-        final Scheduler<Object> subject = new Scheduler<>(Strategy.linear(1), new Scheduler<>(Strategy.linear(1), new Scheduler<>(Strategy.linear(1), aggregator)));
-        final List<?> input = newMessages(1000);
+        final Scheduler<Object> subject = Scheduler.builder(strategy, aggregator).build();
+        final List<?> input = newMessages(size);
 
         input.forEach(subject);
         subject.join(Long.MAX_VALUE);
@@ -25,7 +24,7 @@ public class SchedulerTest {
         );
     }
 
-    private List<?> newMessages(final int size) {
+    private static List<?> newMessages(final int size) {
         final List<Integer> result = new ArrayList<>(size);
         for (int index = 0; index < size; ++index) {
             result.add(index);
@@ -33,7 +32,13 @@ public class SchedulerTest {
         return result;
     }
 
-    private class Aggregator implements Consumer<Object> {
+    @Test
+    public final void testAccept() throws InterruptedException {
+        testAccept(Strategy.linear(1), 1000);
+        testAccept(Strategy.linear(1000), 1);
+    }
+
+    private static class Aggregator implements Consumer<Object> {
         private final List<Object> accepted = new ArrayList<>(0);
 
         @Override
